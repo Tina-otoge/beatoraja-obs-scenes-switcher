7d6f399cd2266589de1f3d8c1d07cc6d430af75c
Sera <42559654+seraxis@users.noreply.github.com>
2024-03-16 14:21:55 +0000

    Update beatoraja.py to support 0.8.7 log lines

--- beatoraja.py
+++ beatoraja.py
@@ -15,11 +15,11 @@ class Result(Scene):
 
 class Quit(Scene):
     pass
-
+    
 SCENE_MATCHES = {
-    'bms.player.beatoraja.MainController$SystemSoundManager shuffle': Select,
+    'bms.player.beatoraja.SystemSoundManager shuffle': Select,
     'bms.player.beatoraja.play.BMSPlayer create': Play,
-    'bms.player.beatoraja.result.MusicResult$IRSendStatus': Result,
+    'bms.player.beatoraja.MainController$IRSendStatus send': Result,
     'alc_cleanup:': Quit,
     'bms.player.beatoraja.MainController dispose': Quit,
 }
